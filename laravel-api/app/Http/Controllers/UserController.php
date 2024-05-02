2f9809641dcaa00cb728837c10f0f453dfad959c
MathisPFR <mathispetitfr@gmail.com>
2024-05-02 09:22:53 +0200

    token delete on new login

--- laravel-api/app/Http/Controllers/UserController.php
+++ laravel-api/app/Http/Controllers/UserController.php
@@ -118,34 +118,45 @@ class UserController extends Controller
     public function loginUser(Request $request)
     {
         try {
-            $validateUser = Validator::make($request->all(), 
-            [
-                'email' => 'required|email',
-                'password' => 'required'
-            ]);
 
-            if($validateUser->fails()){
+
+                $validateUser = Validator::make($request->all(), 
+                [
+                    'email' => 'required|email',
+                    'password' => 'required'
+                ]);
+
+                if($validateUser->fails()){
+                    return response()->json([
+                        'status' => false,
+                        'message' => 'validation error',
+                        'errors' => $validateUser->errors()
+                    ], 401);
+                }
+
+                if(!Auth::attempt($request->only(['email', 'password']))){
+                    return response()->json([
+                        'status' => false,
+                        'message' => 'Email & Password does not match with our record.',
+                    ], 401);
+                }
+
+                $user = User::where('email', $request->email)->first();
+
+                if(auth('sanctum')->check()){
+                    auth()->user()->tokens()->delete();
+    
+                }
+
                 return response()->json([
-                    'status' => false,
-                    'message' => 'validation error',
-                    'errors' => $validateUser->errors()
-                ], 401);
-            }
+                    'status' => true,
+                    'message' => 'User Logged In Successfully',
+                    'token' => $user->createToken("API TOKEN")->plainTextToken
+                ], 200);
 
-            if(!Auth::attempt($request->only(['email', 'password']))){
-                return response()->json([
-                    'status' => false,
-                    'message' => 'Email & Password does not match with our record.',
-                ], 401);
-            }
 
-            $user = User::where('email', $request->email)->first();
-
-            return response()->json([
-                'status' => true,
-                'message' => 'User Logged In Successfully',
-                'token' => $user->createToken("API TOKEN")->plainTextToken
-            ], 200);
+                
+    
 
         } catch (\Throwable $th) {
             return response()->json([
