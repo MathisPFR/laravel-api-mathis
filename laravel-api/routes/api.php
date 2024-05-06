1bfe1f64c7c0d6b1564b3ec2c93feef3756d2aed
MathisPFR <mathispetitfr@gmail.com>
2024-05-06 10:00:58 +0200

    save because next step is suc*de

--- laravel-api/routes/api.php
+++ laravel-api/routes/api.php
@@ -26,29 +26,56 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     'create', 'show', 'edit'
 // ]);
 
-Route::get('/products', [ProductsApiController::class, 'index'])->middleware('auth:sanctum');
-Route::post('/products', [ProductsApiController::class, 'store'])->middleware('auth:sanctum');
-Route::put('/products/{id}', [ProductsApiController::class, 'update'])->middleware('auth:sanctum');
-Route::delete('/products/{id}', [ProductsApiController::class, 'destroy'])->middleware('auth:sanctum');
-Route::get('/products/{id}', [ProductsApiController::class, 'show'])->middleware('auth:sanctum');
+// Route::get('/products', [ProductsApiController::class, 'index'])->middleware('auth:sanctum');
+// Route::post('/products', [ProductsApiController::class, 'store'])->middleware('auth:sanctum');
+// Route::put('/products/{id}', [ProductsApiController::class, 'update'])->middleware('auth:sanctum');
+// Route::delete('/products/{id}', [ProductsApiController::class, 'destroy'])->middleware('auth:sanctum');
+// Route::get('/products/{id}', [ProductsApiController::class, 'show'])->middleware('auth:sanctum');
 
 
 
-Route::get('/categories', [CategoryController::class, 'index'])->middleware('auth:sanctum');
-Route::post('/categories', [CategoryController::class, 'store'])->middleware('auth:sanctum');
-Route::put('/categories/{id}', [CategoryController::class, 'update'])->middleware('auth:sanctum');
-Route::delete('/categories/{id}', [CategoryController::class, 'destroy'])->middleware('auth:sanctum');
-Route::get('/categories/{id}', [CategoryController::class, 'show'])->middleware('auth:sanctum');
+// Route::get('/categories', [CategoryController::class, 'index'])->middleware('auth:sanctum');
+// Route::post('/categories', [CategoryController::class, 'store'])->middleware('auth:sanctum');
+// Route::put('/categories/{id}', [CategoryController::class, 'update'])->middleware('auth:sanctum');
+// Route::delete('/categories/{id}', [CategoryController::class, 'destroy'])->middleware('auth:sanctum');
+// Route::get('/categories/{id}', [CategoryController::class, 'show'])->middleware('auth:sanctum');
 
-// Route::apiResource('products', ProductsApiController::class);
-
-
-Route::get('/users', [UserController::class, 'index'])->middleware('auth:sanctum');
-// Route::post('/users', [UserController::class, 'store'])->middleware('auth:sanctum');
-Route::get('/users/{id}', [UserController::class, 'show'])->middleware('auth:sanctum');
-Route::put('/users/{id}', [UserController::class, 'update'])->middleware('auth:sanctum');
-Route::delete('/users/{id}', [UserController::class, 'destroy'])->middleware('auth:sanctum');
+// // Route::apiResource('products', ProductsApiController::class);
 
 
+// Route::get('/users', [UserController::class, 'index'])->middleware('auth:sanctum');
+// // Route::post('/users', [UserController::class, 'store'])->middleware('auth:sanctum');
+// Route::get('/users/{id}', [UserController::class, 'show'])->middleware('auth:sanctum');
+// Route::put('/users/{id}', [UserController::class, 'update'])->middleware('auth:sanctum');
+// Route::delete('/users/{id}', [UserController::class, 'destroy'])->middleware('auth:sanctum');
+
+
+// Route::post('/auth/register', [UserController::class, 'createUser']);
+// Route::post('/auth/login', [UserController::class, 'loginUser']);
+
+
+
+
+
+Route::prefix('v1')->middleware('auth:sanctum')->group(function () {
+    Route::get('/products', [ProductsApiController::class, 'index']);
+    Route::post('/products', [ProductsApiController::class, 'store']);
+    Route::put('/products/{id}', [ProductsApiController::class, 'update']);
+    Route::delete('/products/{id}', [ProductsApiController::class, 'destroy']);
+    Route::get('/products/{id}', [ProductsApiController::class, 'show']);
+
+    Route::get('/categories', [CategoryController::class, 'index']);
+    Route::post('/categories', [CategoryController::class, 'store']);
+    Route::put('/categories/{id}', [CategoryController::class, 'update']);
+    Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+    Route::get('/categories/{id}', [CategoryController::class, 'show']);
+
+    Route::get('/users', [UserController::class, 'index']);
+    Route::get('/users/{id}', [UserController::class, 'show']);
+    Route::put('/users/{id}', [UserController::class, 'update']);
+    Route::delete('/users/{id}', [UserController::class, 'destroy']);
+});
+
+// Routes pour l'authentification
 Route::post('/auth/register', [UserController::class, 'createUser']);
 Route::post('/auth/login', [UserController::class, 'loginUser']);
